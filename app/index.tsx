2dc9443ad2afa77bd5e841ce11c631e3aba322ae
TTaylorOlsenn <0325055@hc.ac.je>
2026-03-19 09:21:47 +0000

    Changed "Submit Assignment" font weight, placed checkbox inside the "View" component

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,19 @@
-import { View } from "react-native";
+import { View } from 'react-native';
 import { Text } from '@/components/ui/text';
-import { Checkbox } from "@/components/ui/checkbox";
+import { Checkbox } from '@/components/ui/checkbox';
 
 export default function HomeScreen() {
   return (
-    <View className="flex flex-1 py-32 bg-background">
-      <View className="border-2 border-white flex flex-row">
-        <View className="flex items-center justify-center w-16"></View>
-          <Checkbox className="border-2"></Checkbox>
-        <View className="flex flex-col">
-          <Text className="font-bold">Submit Assignment</Text>
-          <Text className="font-semibold">Due: Oct 20</Text>
+    <View className="bg-background flex flex-1 py-32">
+      <View className="flex flex-row border-2 border-white">
+        <View className="flex w-16 items-center justify-center">
+          <Checkbox className="border-2" />
         </View>
-</View>
-</View>
+        <View className="flex flex-col">
+          <Text className="">Submit Assignment</Text>
+          <Text className="">Due: Oct 20</Text>
+        </View>
+      </View>
+    </View>
   );
 }
