7b89d4d85a2e7ee783ac9e0b9a0c6a5a66572a15
TTaylorOlsenn <0325055@hc.ac.je>
2026-05-07 11:57:04 +0100

    Added task tests
    Tests if component renders and if checkbox toggles as expected

--- components/Task.tsx
+++ components/Task.tsx
@@ -36,6 +36,7 @@ export default function Task({ task: initialTask, onUpdate }: TaskProps) {
           onPress={() => setShowDialog(true)}>
           <View className="flex h-full w-24 px-8 py-5">
             <Checkbox
+              testID="checkbox"
               className="border-foreground checked:bg-foreground"
               checked={task.isChecked}
               onCheckedChange={handleSetChecked}
---
+++ tests/Task.test.js
@@ -0,0 +1,52 @@
+import { render, screen, userEvent } from '@testing-library/react-native';
+import Task from '../components/Task';
+
+describe('Task', () => {
+  test('renders a task', () => {
+    const task = {
+      id: 1,
+      title: 'Test Task',
+      category: 'Test Category',
+      isChecked: false,
+    };
+
+    render(<Task task={task} />);
+
+    // Just check if the title and category are displayed
+    const titleElement = screen.getByText('Test Task');
+    const categoryElement = screen.getByText('Test Category');
+    expect(titleElement).toBeTruthy();
+    expect(categoryElement).toBeTruthy();
+  });
+
+  test('toggles completion status when pressed', async () => {
+    const task = {
+      id: 2,
+      title: 'Test Task',
+      category: 'Test Category',
+      isChecked: false,
+    };
+
+    render(<Task task={task} />);
+
+    const checkbox = screen.getByTestId('checkbox'); // Find the checkbox element
+
+    const user = userEvent.setup();
+    await user.press(checkbox);
+
+    expect(checkbox).toBeChecked();
+  });
+
+  test('toggles from checked to unchecked when pressed', async () => {
+    const task = {
+      id: 3,
+      isChecked: false,
+    };
+
+    render(<Task task={task} />);
+
+    const checkbox = screen.getByTestId('checkbox');
+
+    expect(checkbox).not.toBeChecked();
+  });
+});
